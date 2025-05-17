2b9cfe1c060eeed6b92c3228e4f3554c8247958c
jayknight7535 <thewitchjay@gmail.com>
2025-05-17 16:13:32 +0000

    first commit

---
+++ src/js/components/TitleCard.jsx
@@ -0,0 +1,12 @@
+const TitleCard = () => {
+    return(
+        <div class="card" style>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <button type="button" class="btn btn-primary">Primary</button>
+            </div>
+        </div>
+    )
+}
+export default TitleCard
